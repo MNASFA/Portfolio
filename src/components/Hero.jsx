78dd06f934b6de40003ef44f65e6b65c1bdcb37a
HOUSSAM <mnasfa.houssam@gmail.com>
2025-10-05 18:29:18 +0100

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+
+import heropic from "../assets/Learning-cuate.svg"
 
 function Hero() {
   return (
-    <h1>test</h1>
+    <div className='hero w-full px-10'>
+      <div className='two_sides max-w-5/6 mx-auto flex items-center justify-between'>
+        <div className="side_one w-2/3">
+
+          <h4 className="text-2xl pb-5 tracking-wider ml-1">Full Stack Developer</h4>
+
+          <h4 className="text-7xl pb-2 tracking-wider font-bold">Hello I'm</h4>
+          <h4 className="text-7xl pb-4 tracking-wider font-bold text-orange-600">Houssam Mnasfa</h4>
+          <p className="text-2xl pb-10 ml-1">
+            Full-stack developer and student at 1337 Khouribga, passionate 
+            about crafting clean code and building innovative 
+            web solutions. With a diploma in software development, 
+            I transform ideas into functional, 
+            user-friendly applications.</p>
+
+          <div className="buttons pb-10 flex items-center">
+            <button className="ml-1 border-3 rounded-4xl py-3 px-9 border-orange-600 text-orange-600 uppercase">Download cv <i class="fa-solid fa-download"></i></button>
+            <a href="#" className="w-fit" ><i class=" text-orange-600 ml-6 text-4xl fa-brands fa-github"></i></a>
+            <a href="#" className="w-fit" ><i class=" text-orange-600 ml-3 text-4xl fa-brands fa-linkedin"></i></a>
+            <a href="#" className="w-fit" ><i class=" text-orange-600 ml-3 text-4xl fa-brands fa-discord"></i></a>
+            <a href="#" className="w-fit" ><i class=" text-orange-600 ml-3 text-4xl fa-brands fa-instagram"></i></a>
+          </div>
+        </div>
+
+        <div className="side_two w-3/5">
+            <img src={heropic} alt="hero pic"/>
+        </div>
+      </div>
+
+      <div className="notice max-w-5/6 mx-auto w-full flex items-center justify-between">
+        <div className="flex items-center mr-3">
+          <h4 className="text-8xl text-orange-600 mr-3">+8</h4>
+          <strong className="text-2xl">Hours of coding<br />per day</strong>
+        </div>
+         <div className="flex items-center mr-3">
+          <h4 className="text-8xl text-orange-600 mr-3">+10</h4>
+          <strong className="text-2xl">Projects<br />completed</strong>
+        </div>
+         <div className="flex items-center mr-3">
+          <h4 className="text-8xl text-orange-600 mr-3">+5</h4>
+          <strong className="text-2xl">Technologies<br />Mastered</strong>
+        </div>
+         <div className="flex items-center mr-3">
+          <h4 className="text-8xl text-orange-600 mr-3">1337</h4>
+          <strong className="text-2xl">Current<br />focus</strong>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,23 +7,23 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false) // For mobile menu
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 navbar text-gray">
-      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+    <nav className="w-full navbar text-gray">
+      <div className="max-w-5/6 mx-auto px-6 flex justify-between items-center">
         {/* Logo / Name */}
-        <h1 className="logo">Hm<span className='font-bold text-orange-600'>.</span></h1>
+        <h4 className="logo text-8xl">Hm<span className='font-bold text-orange-600'>.</span></h4>
 
         {/* Desktop Links */}
         <ul className="hidden md:flex space-x-6">
-          <li><a href="#hero" className=" hover:text-orange-600">Home</a></li>
-          <li><a href="#about" className=" hover:text-orange-600">About</a></li>
-          <li><a href="#projects" className=" hover:text-orange-600">Projects</a></li>
-          <li><a href="#contact" className=" hover:text-orange-600">Skills</a></li>
-          <li><a href="#contact" className=" hover:text-orange-600">Contact</a></li>
+          <li><a href="#hero" className="text-3xl hover:text-orange-600 text-orange-600 px-2 activelink">Home</a></li>
+          <li><a href="#about" className="text-3xl hover:text-orange-600 px-3">About</a></li>
+          <li><a href="#projects" className="text-3xl hover:text-orange-600 px-3">Projects</a></li>
+          <li><a href="#contact" className="text-3xl hover:text-orange-600 px-3">Skills</a></li>
+          <li><a href="#contact" className="text-3xl hover:text-orange-600 px-3">Contact</a></li>
         </ul>
 
         {/* Mobile Menu Button */}
         <button
-          className="md:hidden text-gray-700"
+          className="md:hidden text-gray"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? "✖" : "☰"}
@@ -34,19 +34,19 @@ function Navbar() {
           <img 
             src={profilepic}
             alt="Profile" 
-            className="h-12 w-12 rounded-full object-cover"
+            className="h-15 w-15 rounded-full object-cover"
           />
         </div>
       </div>
 
       {/* Mobile Menu */}
       {isOpen && (
-        <ul className="md:hidden bg-white shadow-md px-6 py-4 space-y-4">
-          <li><a href="#hero" className="text-gray-700 hover:text-orange-600">Home</a></li>
-          <li><a href="#about" className="text-gray-700 hover:text-orange-600">About</a></li>
-          <li><a href="#projects" className="text-gray-700 hover:text-orange-600">Projects</a></li>
-          <li><a href="#contact" className="text-gray-700 hover:text-orange-600">Skills</a></li>
-          <li><a href="#contact" className="text-gray-700 hover:text-orange-600">Contact</a></li>
+        <ul className="md:hidden bg-orange-600 shadow-md px-10 py-6 space-y-4">
+          <li><a href="#hero" className="text-gray hover:text-neutral-950">Home</a></li>
+          <li><a href="#about" className="text-gray hover:text-neutral-950">About</a></li>
+          <li><a href="#projects" className="text-gray hover:text-neutral-950">Projects</a></li>
+          <li><a href="#contact" className="text-gray hover:text-neutral-950">Skills</a></li>
+          <li><a href="#contact" className="text-gray hover:text-neutral-950">Contact</a></li>
         </ul>
       )}
 
